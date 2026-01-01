ba263f5ea3dc1818c7a61b50acec52fe2cce02ca
Jonas Bernoulli <jonas@bernoul.li>
2026-01-01 19:25:04 +0100

    Release version 1.2.2

--- backline.el
+++ backline.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/tarsius/backline
 ;; Keywords: outlines
 
-;; Package-Version: 1.2.1
+;; Package-Version: 1.2.2
 ;; Package-Requires: (
 ;;     (emacs              "27.1")
 ;;     (compat             "30.1")
